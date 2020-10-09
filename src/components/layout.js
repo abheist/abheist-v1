90f020348cbff89f7a591091545df7ad2ff35f20
abheist <abhishek291093@gmail.com>
2020-10-09 17:18:21 +0530

    👨‍💻 OCD thingy

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Abhishek Kumar Singh
+        &copy; {new Date().getFullYear()}, Abhishek Kumar Singh
         {` `}🤘
       </footer>
     </div>
