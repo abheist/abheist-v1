0e80f4f74fc79ac542ff47c3758aa5c226147f1f
abheist <abhishek291093@gmail.com>
2020-12-31 14:20:43 +0530

    📦 added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
