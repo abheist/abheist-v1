f41ed27c7a3917aa9c82057b966d2122223d3b3f
abheist <abhishek291093@gmail.com>
2021-01-06 18:45:45 +0530

    ✨ Fixed Navigation padding

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,7 +5,7 @@ import Logo from './Logo'
 const Navigation = ({ title }) => {
   return (
     <div className="container px-4 mx-auto">
-      <nav className="flex flex-row justify-between w-full p-8">
+      <nav className="flex flex-row justify-between w-full py-8">
         <Logo title={title} />
         <div className="flex flex-row items-center justify-end gap-x-32">
           <div>
