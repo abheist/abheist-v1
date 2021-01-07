93aaee5359b0b4f044f80dec140b0ba60e664f7b
abheist <abhishek291093@gmail.com>
2021-01-07 22:52:10 +0530

    🚧 intermediate

--- src/components/mdx/li.tsx
+++ src/components/mdx/li.tsx
@@ -3,15 +3,13 @@ import * as React from 'react'
 const Li = ({ children, check }) => {
   return (
     <li
-      //   // [bpMaxSM]: {
-      //   //   paddingLeft: check ? '35px' : '0',
-      //   // },
-      css={`
-        verticalalign: middle;
-        liststyletype: ${check ? 'none' : 'circle'}
-        background: ${check && 'url(./check.svg) no-repeat 0 2px transparent'};
-        paddingLeft: ${check ? '40px' : '0'}
-      `}
+      className="pl-40 align-middle list-disc bg-red-400"
+      // style={`
+      //   verticalalign: middle;
+      //   liststyletype: ${check ? 'none' : 'circle'}
+      //   background: ${check && 'url(./check.svg) no-repeat 0 2px transparent'};
+      //   paddingLeft: ${check ? '40px' : '0'}
+      // `}
     >
       {children}
     </li>
