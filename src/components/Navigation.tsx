4807a4f0ddb072468c00df35c0eed7d40e6c07d5
abheist <abhishek291093@gmail.com>
2021-01-15 16:34:05 +0530

    🚧 Navigation: Currently selected page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,26 +3,33 @@ import React from 'react'
 import Logo from './Logo'
 
 const Navigation = ({ title }) => {
+  const activeStyle = 'text-indigo-500 font-bold'
+
+  function pathIncludes(word: string) {
+    return window.location.href.includes(word) ? activeStyle : ''
+  }
+
+  const pages = [
+    { name: 'Articles', path: '/articles/' },
+    { name: 'Book Notes', path: '/book-notes/' },
+    { name: 'About Me', path: '/me/' },
+  ]
+
   return (
     <div className="container px-4 mx-auto">
       <nav className="flex flex-row justify-between w-full py-8">
         <Logo title={title} />
         <div className="flex flex-row items-center justify-end gap-x-32">
-          <div>
-            <Link to="/articles/" className="text-sm">
-              Articles
-            </Link>
-          </div>
-          <div>
-            <Link to="/book-notes/" className="text-sm">
-              Book Notes
-            </Link>
-          </div>
-          <div>
-            <Link to="/me/" className="text-sm">
-              About Me
-            </Link>
-          </div>
+          {pages.map(page => (
+            <div>
+              <Link
+                to={page.path}
+                className={`text-sm ${pathIncludes(page.path)}`}
+              >
+                {page.name}
+              </Link>
+            </div>
+          ))}
         </div>
       </nav>
     </div>
