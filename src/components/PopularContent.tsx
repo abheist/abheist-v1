9c5fde490a649870402e2ee5276b912b711df470
abheist <abhishek291093@gmail.com>
2021-01-18 21:52:29 +0530

    🚧 About me, link hover ndigo

--- src/components/PopularContent.tsx
+++ src/components/PopularContent.tsx
@@ -9,7 +9,9 @@ const PopularContent = ({ posts, heading }) => {
       {posts.map(post => (
         <Subtitle1 key={post.fields.slug} className="mt-4">
           <Link to={post.fields.slug} itemProp="url">
-            <span itemProp="headline">{post.frontmatter.title}</span>
+            <span itemProp="headline" className="hover:text-indigo-primary">
+              {post.frontmatter.title}
+            </span>
           </Link>
         </Subtitle1>
       ))}
