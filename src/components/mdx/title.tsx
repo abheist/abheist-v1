e02bc164c6017d30640a79968fdd78af6dc743e2
abheist <abhishek291093@gmail.com>
2021-01-24 02:18:53 +0530

    ⛑ typography fixed

--- src/components/mdx/title.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export default styled.h1`
-  line-height: 1.35;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  padding-top: 30px;
-  padding-bottom: 10px;
-`
