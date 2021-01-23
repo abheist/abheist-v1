1486a7161f1c8294fe78286e52c188c08985a637
abheist <abhishek291093@gmail.com>
2021-01-23 14:17:35 +0530

    ⛑ Responsiveness: PicPageSection done

--- src/components/PicPageSection.tsx
+++ src/components/PicPageSection.tsx
@@ -42,13 +42,13 @@ const PicPageSection = ({
 }: PicPageSectionProps) => {
   return (
     <div className={backgroundColor}>
-      <Container className="py-24">
-        <div className="flex flex-row justify-between w-full gap-32">
+      <Container className="pt-0 lg:pt-24 pb-24">
+        <div className="flex flex-col lg:flex-row justify-between w-full lg:gap-32">
           <Link
             to={latestArticle.to}
-            className={`flex flex-row items-end justify-end flex-shrink-0 ${
-              picSide === 'left' ? 'order-1' : 'order-2'
-            } w-full bg-blue-400 md:w-1/2`}
+            className={`flex flex-row items-end justify-end flex-shrink-0 order-2 ${
+              picSide === 'left' ? 'lg:order-1' : 'lg:order-2'
+            } w-full bg-blue-400 lg:w-1/2`}
             style={{
               height: '400px',
               background: `url(${latestArticle.pic.childImageSharp.fluid.src})`,
@@ -59,15 +59,18 @@ const PicPageSection = ({
             }}
           >
             <div
-              className={`p-10 pr-8 -mr-4 bg-white -mb-28 ${backgroundColor}`}
-              style={{ width: '450px' }}
+              className={`p-10 lg:pr-8 lg:-mr-4 bg-white bg-opacity-90 lg:bg-opacity-100 lg:w-96 lg:-mb-28 ${backgroundColor}`}
             >
               <Overline className="mb-2">{latestArticle.overline}</Overline>
               <H3 className="text-indigo-primary">{latestArticle.heading}</H3>
               <Body1 className="mt-8">{latestArticle.description}</Body1>
             </div>
           </Link>
-          <div className={`${picSide === 'left' ? 'order-2' : 'order-1'} py-8`}>
+          <div
+            className={`order-1 ${
+              picSide === 'left' ? 'lg:order-2' : 'lg:order-1'
+            } py-8`}
+          >
             <H3>{heading}</H3>
             <Body1 className="mt-8">{description}</Body1>
             {to && linkName && (
@@ -85,7 +88,7 @@ const PicPageSection = ({
         {featured && (
           <div className="mt-44">
             <H6>FEATURED</H6>
-            <div className="flex flex-row justify-between w-full gap-8 mt-8">
+            <div className="grid grid-col-1 lg:grid-cols-3 gap-8 mt-8">
               {featured.map(card => (
                 <Card key={card.fields.slug} data={card} />
               ))}
