41d9b8941561d675e3c8b0344c90925c6c1cab14
abheist <abhishek291093@gmail.com>
2021-01-26 22:46:53 +0530

    🚧 slip

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -4,26 +4,21 @@ import { Body1, H2 } from './Typography'
 
 const HomeHeader = ({ avatar }) => {
   return (
-    <div
-      className="flex flex-col justify-end w-full
-                  lg:flex-row lg:items-end"
-    >
+    <div className="flex flex-col justify-end w-full lg:flex-row lg:items-end">
       <div
-        className="z-10 p-2 mt-8 order-2 
-                  lg:p-10 lg:pl-0 lg:-mb-56 lg:bg-white lg:-mr-64 lg:order-1
-                  xl:-mb-32 2xl:-mr-96"
+        className="z-10 order-2 p-2 mt-8 lg:p-10 lg:pl-0 lg:-mb-56 lg:bg-white lg:-mr-64 lg:order-1 xl:-mb-32 2xl:-mr-96"
         style={{ maxWidth: '560px' }}
       >
         <H2>
           Hi, I'm <span className="strike">Abhishek</span> a Product Developer
         </H2>
         <Body1 className="mt-8">
-          I love exploring ideas, strategies & tools to develop a good software.
-          Expore more about happiness, health and more product life.
+          who has a fair experience as a designer and loves to explore ideas,
+          strategies and more about health, happiness & more productive life.
         </Body1>
       </div>
       <Img
-        className="w-full lg:w-2/3 order-1 lg:order-2"
+        className="order-1 w-full lg:w-2/3 lg:order-2"
         style={{
           boxShadow: '-16px 16px  var(--primary-color)',
         }}
