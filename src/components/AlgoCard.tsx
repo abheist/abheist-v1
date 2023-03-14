c8aecfeb039b1910c28e8c82047b763167b600a6
abheist <abhishek291093@gmail.com>
2023-03-14 01:53:52 -0300

    📚 algo card color change based on dificulty

--- src/components/AlgoCard.tsx
+++ src/components/AlgoCard.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'gatsby'
-import React from 'react'
-import { Overline, Subtitle1 } from './Typography'
+import { Link } from "gatsby";
+import React from "react";
+import { Overline, Subtitle1 } from "./Typography";
 
 const AlgoCard = ({ algo }) => {
   return (
@@ -8,12 +8,12 @@ const AlgoCard = ({ algo }) => {
       className="group border border-l-8 px-4 py-4 hover:shadow-sm"
       style={{
         borderLeftColor: `${
-          algo.frontmatter.difficulty === 'easy'
-            ? 'rgba(16, 185, 129)'
-            : algo.frontmatter.difficulty === 'medium'
-            ? 'rgba(59, 130, 246)'
-            : 'rgba(239, 68, 68)'
-        }`,
+          algo.frontmatter.difficulty === "easy"
+            ? "rgba(16, 185, 129)"
+            : algo.frontmatter.difficulty === "medium"
+              ? "rgba(59, 130, 246)"
+              : "rgba(239, 68, 68)"
+        }`
       }}
     >
       <Link to={algo.fields.slug}>
@@ -27,13 +27,21 @@ const AlgoCard = ({ algo }) => {
           >
             {algo.frontmatter.title}
           </Subtitle1>
-          <Overline className="rounded-xl bg-green-300 px-2 py-0.5">
+          <Overline className="rounded-xl bg-green-300 px-2 py-0.5" style={{
+            backgroundColor: `${
+              algo.frontmatter.difficulty === "easy"
+                ? "rgba(16, 185, 129)"
+                : algo.frontmatter.difficulty === "medium"
+                  ? "rgba(59, 130, 246)"
+                  : "rgba(239, 68, 68)"
+            }`
+          }}>
             {algo.frontmatter.category}
           </Overline>
         </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default AlgoCard
+export default AlgoCard;
