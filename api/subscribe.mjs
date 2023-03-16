c7887da811fca090186a9b44c6e54ea1217c57b8
abheist <abhishek291093@gmail.com>
2023-03-16 12:22:46 -0300

    📚 fixed subscribe.mjs

--- api/subscribe.mjs
+++ api/subscribe.mjs
@@ -1,34 +1,44 @@
-// import type { VercelRequest, VercelResponse } from '@vercel/node'
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
 export default async (request, response) => {
-  const { REVUE_API_KEY } = process.env
-  const url = `https://www.getrevue.co/api/v2/subscribers`
+  const { CONVERTKIT_API_KEY } = process.env;
 
-  const body = JSON.parse(request.body)
-  const email = body.email
+  const body = JSON.parse(request.body);
+  const email = body.email;
 
+  const res = await subscribeToConvertKit(email, CONVERTKIT_API_KEY);
+  return res.status(200).send({ statusCode: 200, body: "success" });
+
+  if (res.error) {
+    console.error(res.error);
+    return res
+      .status(200)
+      .send({ statusCode: 500, body: JSON.stringify(res.error.message) });
+  }
+}
+
+export default async function subscribeToConvertKit(email, apiKey) {
   try {
-    await fetch(url, {
-      method: 'POST',
+    const response = await fetch(`https://api.convertkit.com/v3/forms/1922220/subscribe`, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Token ${REVUE_API_KEY}`,
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        email,
-        double_opt_in: false,
-      }),
-    })
-      .then(res => res.json())
-      .then(res => console.log(res))
-      .catch(error => {
-        throw new Error(error)
+        email: email,
+        api_key: apiKey
       })
-    return response.status(200).send({ statusCode: 200, body: 'success' })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    return data;
   } catch (error) {
-    return response
-      .status(200)
-      .send({ statusCode: 500, body: JSON.stringify(error.message) })
+    console.error(error);
+    return { error: error.message };
   }
 }
